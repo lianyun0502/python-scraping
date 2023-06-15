010fe7f228a38240b9d1a341c5642fadbc5f627a
ERICLI-NB\eric.li <eric.li@kaikutek.com>
2023-06-15 18:53:01 +0800

    Edit selenium operation .md

--- v1/chapter9/1-simpleForm.py
+++ v1/chapter9/1-simpleForm.py
@@ -1,5 +1,5 @@
 import requests
 
 params = {'firstname': 'Ryan', 'lastname': 'Mitchell'}
-r = requests.post("http://pythonscraping.com/pages/processing.php", params=params)
+r = requests.post("https://pythonscraping.com/pages/processing.php", params=params)
 print(r.text)
